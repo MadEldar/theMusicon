edc45b547386249f275a47b08f79c87bbc603b08
MadEldar1 <markskilink@gmail.com>
2020-03-30 17:01:02 +0700

    Template integrated

---
+++ resources/views/musicon/partials/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- Title -->
+    <title>One Music - Modern Music HTML5 Template</title>
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Archivo+Narrow:400,400i,500,500i,600,600i,700,700i">
+    <link rel="stylesheet" href="{{ asset('musicon/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/classy-nav.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/audioplayer.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/one-music-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('musicon/css/style.css') }}">
+</head>
---
+++ resources/views/musicon/partials/scripts.blade.php
@@ -0,0 +1,10 @@
+<!-- jQuery-2.2.4 js -->
+<script src="{{ asset('musicon/js/jquery/jquery-2.2.4.min.js') }}"></script>
+<!-- Popper js -->
+<script src="{{ asset('musicon/js/bootstrap/popper.min.js') }}"></script>
+<!-- Bootstrap js -->
+<script src="{{ asset('musicon/js/bootstrap/bootstrap.min.js') }}"></script>
+<!-- All Plugins js -->
+<script src="{{ asset('musicon/js/plugins/plugins.js') }}"></script>
+<!-- Active js -->
+<script src="{{ asset('musicon/js/active.js') }}"></script>
