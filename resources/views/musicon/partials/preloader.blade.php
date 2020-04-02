993d4a09e0dee6ebeedf422087cd6b43a47d741b
MadEldar1 <markskilink@gmail.com>
2020-04-03 00:32:25 +0700

    Finished user sign in

--- resources/views/musicon/partials/preloader.blade.php
+++ resources/views/musicon/partials/preloader.blade.php
@@ -1,8 +1,8 @@
-{{--<div class="preloader d-flex align-items-center justify-content-center">--}}
-{{--    <div class="lds-ellipsis">--}}
-{{--        <div></div>--}}
-{{--        <div></div>--}}
-{{--        <div></div>--}}
-{{--        <div></div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+<div class="preloader d-flex align-items-center justify-content-center">
+    <div class="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
